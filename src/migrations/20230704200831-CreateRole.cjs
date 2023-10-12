c237a19a0283b16b4439e26af262b5d05e77b423
Denniel <dennielwilliam.unb@gmail.com>
2023-10-12 01:54:08 -0300

    Ajustando build para realizar a migration

--- src/migrations/20230704200831-CreateRole.cjs
+++ src/migrations/20230704200831-CreateRole.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('note', {
+    await queryInterface.createTable('role', {
       idRole: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -33,6 +33,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('note');
+    await queryInterface.dropTable('role');
   },
 };
